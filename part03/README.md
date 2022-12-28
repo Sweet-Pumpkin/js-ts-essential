21b9361e2fe110faf3f999326bdc4d5afe1f057d
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-12-28 17:28:08 +0100

    feat: Add paging

--- part03/README.md
+++ part03/README.md
@@ -18,3 +18,58 @@ ajax.open('GET', 'URL', 'Boolean');
 ajax.send();
 JSON.parse(ajax.response);
 ```
+
+#### 4. location 객체
+[참고 링크](https://ffoorreeuunn.tistory.com/167)
+- 문서의 주소와 관련된 객체로 window 객체의 프로퍼티.
+- 이 객체로 윈도우 문서의 url을 변경, 문서의 위치와 관련해 다양한 정보를 얻을 수 있음.
+
+#### 5. DOM API => 문자열로 바꾸기 
+##### DOM API
+```
+for (let i = 0; i < 10 i++) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+
+    a.href = `#${newsFeed[i].id}`;
+    a.innerHTML = `${newsFeed[i].title (${newsFeed[i].comments_count})}`;
+
+    li.appendChild(a);
+    ul.appendChild(li);
+} 
+```
+
+##### 문자열1
+```
+for (let i = 0; i < 10 i++) {
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+        <li>
+            <a href="#${newFeed[i].id}">
+                ${newsFeed[i].title (${newsFeed[i].comments_count})}             
+            </a>
+        </li>
+    `
+
+    ul.appendChild(div.children[0]);
+}
+```
+
+##### 문자열2
+```
+const newsList = [];
+newsList.push('<ul>');
+for (let i = 0; i < 10; i++) {
+    
+    newsList.push(`
+        <li>
+            <a href="#${newsFeed[i].id}">
+                ${newsFeed[i].title} (${newsFeed[i].comments_count})
+            </a>
+        </li>
+    `);
+}
+newsList.push('</ul>');
+container.innerHTML = newsList.join('');
+```
--- part03/app.js
+++ part03/app.js
@@ -8,6 +8,9 @@ const CONTENTS_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
 /** 탐색 & 선언 */
 const container = document.getElementById('root');
+const store = {
+    currentPage: 1,
+}
 
 /** 함수 */
 // Hacker News API 가져오는 함수
@@ -22,27 +25,38 @@ function getDataFnc(URL) {
 function newsFeedFnc() {
     // 뉴스 목록 가져오기
     const newsFeed = getDataFnc(NEWS_URL);
-    // 출력
+    // 한 페이지 당 뉴스 목록
+    const PAGE_ELS = 10;
+    // 마지막 뉴스 목록 페이지 수
+    const lastNewsFeed = newsFeed.length / PAGE_ELS;
+    // 뉴스 목록
     const newsList = [];
     newsList.push('<ul>');
-    for (let i = 0; i < 10; i++) {
-        
+    for (let i = (store.currentPage - 1) * PAGE_ELS; i < store.currentPage * PAGE_ELS; i++) {
         newsList.push(`
             <li>
-                <a href="#${newsFeed[i].id}">
+                <a href="#/show/${newsFeed[i].id}">
                     ${newsFeed[i].title} (${newsFeed[i].comments_count})
                 </a>
             </li>
         `);
     }
     newsList.push('</ul>');
+    // 네비게이션
+    newsList.push(`
+        <div>
+            <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+            <a href="#/page/${store.currentPage <  lastNewsFeed ? store.currentPage + 1 : lastNewsFeed}">다음 페이지</a>
+        </div>
+    `);
+    // 출력
     container.innerHTML = newsList.join('');
 }
 // 뉴스 콘텐츠 출력 함수
 function newsDetailFnc() {
     // 콘텐츠 아이디 추출 
     // '#1234'로 출력됨 => '#' 제거(substring)
-    const id = location.hash.substring(1);
+    const id = location.hash.substring(7);
     // url의 마킹 '@id'를 replace() 메서드를 이용해 'id'로 변경
     const newsContent = getDataFnc(CONTENTS_URL.replace('@id', id));
 
@@ -50,7 +64,7 @@ function newsDetailFnc() {
     container.innerHTML = `
         <h1>${newsContent.title}</h1>
         <div>
-            <a href="#">목록으로</a>
+            <a href="#/page/${store.currentPage}">목록으로</a>
         </div>
     `;
 }
@@ -60,6 +74,10 @@ function routerFnc() {
 
     if (routePath === '') {
         // 해시 값이 없을 경우 => 목록
+        newsFeedFnc();
+    } else if (routePath.indexOf('#/page/') >= 0) {
+        // 해시 값에 '#/page/'가 0보다 클 때 => 목록 페이지
+        store.currentPage = Number(routePath.substring(7));
         newsFeedFnc();
     } else {
         // 해시 값이 있을 경우 => 콘텐츠
