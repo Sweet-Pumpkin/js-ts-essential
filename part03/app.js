62ac852ed6ee7da715cad79b8a14d0045a4ad046
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-12-28 15:09:07 +0100

    refactor: app.js

--- part03/app.js
+++ part03/app.js
@@ -6,14 +6,22 @@ const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 // @id => 마킹!
 const CONTENTS_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-/** GET Hacker News API */
-// open(1, 2, 3); => 1: string, 2: url, 3: boolean(비동기 or 동기 처리 유무)
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
-const newsFeed = JSON.parse(ajax.response);
+/** 함수 */
+// GET Hacker News API
+function getData(URL) {
+    // open(1, 2, 3); => 1: string, 2: url, 3: boolean(비동기 or 동기 처리 유무)
+    ajax.open('GET', URL, false);
+    ajax.send();
+
+    return JSON.parse(ajax.response);
+}
 
 /** 탐색 & 선언 */
+// html 요소
 const container = document.getElementById('root');
+// 뉴스 목록 가져오기
+const newsFeed = getData(NEWS_URL);
+// elements
 const content = document.createElement('div');
 const ul = document.createElement('ul');
 
@@ -24,10 +32,8 @@ window.addEventListener('hashchange', function() {
     // '#1234'로 출력됨 => '#' 제거(substring)
     const id = location.hash.substring(1);
     // url의 마킹 '@id'를 replace() 메서드를 이용해 'id'로 변경
-    ajax.open('GET', CONTENTS_URL.replace('@id', id), false);
-    ajax.send();
-    const newsContent = JSON.parse(ajax.response);
-    
+    const newsContent = getData(CONTENTS_URL.replace('@id', id));
+
     // 뉴스 타이틀 출력
     const title = document.createElement('h1');
     title.innerHTML = newsContent.title;
@@ -36,14 +42,19 @@ window.addEventListener('hashchange', function() {
 
 /** 출력 */
 for (let i = 0; i < 10; i++) {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
+    // 임시 태그
+    const div = document.createElement('div');
     
-    a.href = `#${newsFeed[i].id}`;
-    a.innerText = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+    div.innerHTML = `
+        <li>
+            <a href="#${newsFeed[i].id}">
+                ${newsFeed[i].title} (${newsFeed[i].comments_count})
+            </a>
+        </li>
+    `
 
-    li.appendChild(a);
-    ul.appendChild(li);
+    // div요소는 필요없으므로 자식 태그 'li' 태그부터 사용
+    ul.appendChild(div.children[0]);
 }
 
 container.appendChild(ul);
