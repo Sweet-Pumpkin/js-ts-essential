5acea318875a4a6b0515512ea516a69dcd2437fd
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-12-30 22:37:30 +0100

    refactor: Migration with TypeScript

--- part04/app.ts
+++ part04/app.ts
@@ -1,5 +1,31 @@
+/** TYPE ALIAS */
+type Store = {
+    currentPage: number;
+    feeds: NewsFeed[];
+}
+type News = {
+    id: number;
+    time_ago: string;
+    title: string;
+    url: string;
+    user: string;
+    content: string;
+}
+type NewsFeed = News & {
+    comments_count: number;
+    points: number;
+    read?: boolean;
+}
+type NewsComment = News & {
+    comments: NewsComment[];
+    level: number;
+}
+type NewsDetail = News & {
+    comments: NewsComment[];
+}
+
 /** AJAX */
-const ajax = new XMLHttpRequest();
+const ajax: XMLHttpRequest = new XMLHttpRequest();
     
 /** URL */
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
@@ -7,37 +33,46 @@ const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENTS_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
 /** 탐색 & 선언 */
-const container = document.getElementById('root');
-const store = {
+const container: HTMLElement | null = document.getElementById('root');
+const store: Store = {
     currentPage: 1,
     feeds: [],
 }
 
 /** 함수 */
 // Hacker News API 가져오는 함수
-function getDataFnc(URL) {
+function getDataFnc<AjaxResponse>(url: string): AjaxResponse {
     // open(1, 2, 3); => 1: string, 2: url, 3: boolean(비동기 or 동기 처리 유무)
-    ajax.open('GET', URL, false);
+    ajax.open('GET', url, false);
     ajax.send();
 
     return JSON.parse(ajax.response);
 }
 // read 데이터 추가 함수
-function makeFeeds(feeds) {
+function makeFeeds(feeds: NewsFeed[]): NewsFeed[] {
     for (let i = 0; i < feeds.length; i++) {
         feeds[i].read = false;
     }
 
     return feeds;
 }
+// TypeScript 문법 상 null container가 null 값이 아닐 경우 설정
+function updateView(html: string): void {
+    if (container) {
+        container.innerHTML = html;
+    } else {
+        console.error('최상위 컨테이너가 없어 UI를 진행하지 못합니다.');
+    }
+}
+
 // 뉴스 피드 출력 함수
-function newsFeedFnc() {
+function newsFeedFnc(): void {
     // 뉴스 목록 가져오기
-    let newsFeed = store.feeds;
+    let newsFeed: NewsFeed[] = store.feeds;
 
     // 최소 실행
     if (newsFeed.length === 0) {
-        newsFeed = store.feeds = makeFeeds(getDataFnc(NEWS_URL));
+        newsFeed = store.feeds = makeFeeds(getDataFnc<NewsFeed[]>(NEWS_URL));
     }
 
     // 한 페이지 당 뉴스 목록
@@ -98,19 +133,19 @@ function newsFeedFnc() {
     // 뉴스 콘텐츠
     template = template.replace('{{__news_feed__}}', newsList.join(''));
     // 이전 페이지
-    template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
+    template = template.replace('{{__prev_page__}}', String(store.currentPage > 1 ? store.currentPage - 1 : 1));
     // 다음 페이지
-    template = template.replace('{{__next_page__}}', store.currentPage < lastNewsFeed ? store.currentPage + 1 : lastNewsFeed);
+    template = template.replace('{{__next_page__}}', String(store.currentPage < lastNewsFeed ? store.currentPage + 1 : lastNewsFeed));
     // 출력
-    container.innerHTML = template;
+    updateView(template);
 }
 // 뉴스 콘텐츠 출력 함수
-function newsDetailFnc() {
+function newsDetailFnc(): void {
     // 콘텐츠 아이디 추출 
     // '#/page/1234' or '#/show/1234'로 출력됨 => '#/page' 제거(substring)
     const id = location.hash.substring(7);
     // url의 마킹 '@id'를 replace() 메서드를 이용해 'id'로 변경
-    const newsContent = getDataFnc(CONTENTS_URL.replace('@id', id));
+    const newsContent = getDataFnc<NewsDetail>(CONTENTS_URL.replace('@id', id));
 
     // 템플릿
     let template = `
@@ -148,38 +183,37 @@ function newsDetailFnc() {
             store.feeds[i].read = true;
             break;
         }
-    }
-
-    // 댓글 출력
-    function makeComment(comments, called = 0) {
-        const commentString = [];
-    
-        for(let i = 0; i < comments.length; i++) {
-          commentString.push(`
-            <div style="padding-left: ${called * 40}px;" class="mt-4">
-              <div class="text-gray-400">
-                <i class="fa fa-sort-up mr-2"></i>
-                <strong>${comments[i].user}</strong> ${comments[i].time_ago}
-              </div>
-              <p class="text-gray-700">${comments[i].content}</p>
-            </div>      
-          `);
-            
-          // 대댓글
-          if (comments[i].comments.length > 0) {
-            // 재귀호출
-            commentString.push(makeComment(comments[i].comments, called + 1));
-          }
-        }
-    
-        return commentString.join('');
-      }
-    
+    }    
     // 페이지 초기화 & 상세 콘텐츠 출력
-    container.innerHTML = template.replace('{{__comments__}}', makeComment(newsContent.comments));
+    updateView(template.replace('{{__comments__}}', makeComment(newsContent.comments)));
+}
+// 댓글 출력 함수
+function makeComment(comments: NewsComment[]): string {
+    const commentString = [];
+
+    for(let i = 0; i < comments.length; i++) {
+        const comment: NewsComment = comments[i];
+
+        commentString.push(`
+        <div style="padding-left: ${comment.level * 40}px;" class="mt-4">
+            <div class="text-gray-400">
+            <i class="fa fa-sort-up mr-2"></i>
+            <strong>${comment.user}</strong> ${comment.time_ago}
+            </div>
+            <p class="text-gray-700">${comment.content}</p>
+        </div>      
+        `);
+        
+        // 대댓글
+        if (comment.comments.length > 0) {
+        // 재귀호출
+        commentString.push(makeComment(comment.comments));
+        }
+    }
+    return commentString.join('');
 }
 // 라우터 함수
-function routerFnc() {
+function routerFnc(): void {
     const routePath = location.hash;
 
     if (routePath === '') {
