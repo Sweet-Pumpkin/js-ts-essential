36f8b1ec35d69da0bf41d191cfdd0f80fa3d9d67
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-12-31 14:42:31 +0100

    refactor: app.ts

--- part04/app.ts
+++ part04/app.ts
@@ -1,27 +1,27 @@
 /** TYPE ALIAS */
-type Store = {
+interface Store {
     currentPage: number;
     feeds: NewsFeed[];
 }
-type News = {
-    id: number;
-    time_ago: string;
-    title: string;
-    url: string;
-    user: string;
-    content: string;
+interface News {
+    readonly id: number;
+    readonly time_ago: string;
+    readonly title: string;
+    readonly url: string;
+    readonly user: string;
+    readonly content: string;
 }
-type NewsFeed = News & {
-    comments_count: number;
-    points: number;
+interface NewsFeed extends News {
+    readonly comments_count: number;
+    readonly points: number;
     read?: boolean;
 }
-type NewsComment = News & {
-    comments: NewsComment[];
-    level: number;
+interface NewsComment extends News {
+    readonly comments: NewsComment[];
+    readonly level: number;
 }
-type NewsDetail = News & {
-    comments: NewsComment[];
+interface NewsDetail extends News {
+    readonly comments: NewsComment[];
 }
 
 /** AJAX */
